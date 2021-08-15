4353c644ff7a6e5c4ca2e39a8e668a20eaa3854e
KhaledAllam <kaaoaa505@gmail.com>
2021-08-15 15:22:08 +0300

    test google rpc client and server protocol buffers

---
+++ GrpcDemo/GrpcClientProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Net.Client;
+using GrpcServerProject;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClientProject
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Client Start.");
+
+            var input = new HelloRequest { Name = "Khaled" };
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
+            Console.WriteLine("Client End.");
+
+            Console.ReadLine();
+        }
+    }
+}
